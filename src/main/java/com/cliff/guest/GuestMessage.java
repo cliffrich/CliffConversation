18753261c30f2894dc711344327afda1b1e8ae9c
cliff <cliffra@gmail.com>
2019-10-05 22:26:53 +0100

    Wifi password updated with TalkTalk password

--- src/main/java/com/cliff/guest/GuestMessage.java
+++ src/main/java/com/cliff/guest/GuestMessage.java
@@ -22,21 +22,21 @@ public class GuestMessage {
 		guestInfo.put("ruby", "Hi ruby Have I got something to eat");
 		guestInfo.put("daniel", "welcome daniel Have you cleaned your ears");
 		guestInfo.put("jay", "welcome jayasingh.Jaysingh is also called the mustache man of reading. A very handsome guy, I can say, he is the heart-throb of reading university.");
-		guestInfo.put("wifi", 
+		guestInfo.put("wifi",
 				"<speak>"
 						+ "<amazon:effect name=\"whispered\">I want to tell you a secret.</amazon:effect><break time=\"2s\"/>"
-						+ "Have you got your pen and paper?<break time=\"5s\"/>"
-						+ "<say-as interpret-as=\"spell-out\">7w7<break time=\"3s\"/>p3v<break time=\"3s\"/>mpk<break time=\"3s\"/>zzj<break time=\"3s\"/>zk6</say-as>."
+						+ "Have you got your pen and paper?<break time=\"5s\" Please note all letters in the password are in capitals/>"
+						+ "<say-as interpret-as=\"spell-out\">8TR<break time=\"3s\"/>3K<break time=\"3s\"/>3K<break time=\"3s\"/>T</say-as>."
 						+ "<emphasis level=\"strong\">hope you noted down the wifi password. if not, ask again</emphasis>"
-				+ "</speak>");
-    }
-	
+						+ "</speak>");
+	}
+
 	public static String aboutTheGuest(String guestName) {
 		if(guestsAndKey.get(guestName) == null)
 			return REPROMPT+guestName;
 		return guestInfo.get(guestsAndKey.get(guestName));
 	}
-	
+
 	public static String getInformationAboutOurHome(String about) {
 		return guestInfo.get(about);
 	}
